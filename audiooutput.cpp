d18a83066889be6f33887d69da60bc68cbe218a8
Reza <rabdoli2002@gmail.com>
2024-10-21 03:45:25 +0330

    audio output

---
+++ audiooutput.cpp
@@ -0,0 +1,28 @@
+#include "audiooutput.h"
+#include <QDebug>
+
+AudioOutput::AudioOutput(const QAudioFormat &format, QObject *parent)
+    : QObject(parent), audioFormat(format) {
+    audioSink = new QAudioSink(audioFormat, this); //QAudioSink
+}
+
+
+AudioOutput::~AudioOutput() {
+    stop();
+}
+
+//
+void AudioOutput::start() {
+    audioSink->start();
+}
+
+
+void AudioOutput::stop() {
+    audioSink->stop();
+}
+
+
+void AudioOutput::addData(const unsigned char *data, int len) {
+    QMutexLocker locker(&mutex);
+    audioSink->write(reinterpret_cast<const char *>(data), len); //QAudioSink
+}
